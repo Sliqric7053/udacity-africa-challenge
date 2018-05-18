6a2e7998289aa51a7ab4b949ac95c43415173630
Sliqric7053 <richardsit@gmail.com>
2018-05-18 20:26:34 +0200

    store stuff in the db

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -1,6 +1,7 @@
-import PostsView from './views/Posts';
-import ToastsView from './views/Toasts';
-import idb from 'idb';
+import PostsView from "./views/Posts";
+import ToastsView from "./views/Toasts";
+import idb from "idb";
+import { Promise } from "es6-promise";
 
 function openDatabase() {
   // If the browser doesn't support service worker,
@@ -8,6 +9,13 @@ function openDatabase() {
   if (!navigator.serviceWorker) {
     return Promise.resolve();
   }
+
+  // return new Promise(() => {
+  return idb.open("wittr", 1, upgradeDb => {
+    let wittrs = upgradeDb.createObjectStore("wittrs", { keyPath: "id" });
+    wittrs.createIndex("by-date", "time");
+  });
+  // });
 
   // TODO: return a promise for a database called 'wittr'
   // that contains one objectStore: 'wittrs'
@@ -31,7 +39,7 @@ IndexController.prototype._registerServiceWorker = function() {
 
   var indexController = this;
 
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+  navigator.serviceWorker.register("/sw.js").then(function(reg) {
     if (!navigator.serviceWorker.controller) {
       return;
     }
@@ -46,7 +54,7 @@ IndexController.prototype._registerServiceWorker = function() {
       return;
     }
 
-    reg.addEventListener('updatefound', function() {
+    reg.addEventListener("updatefound", function() {
       indexController._trackInstalling(reg.installing);
     });
   });
@@ -54,7 +62,7 @@ IndexController.prototype._registerServiceWorker = function() {
   // Ensure refresh is only called once.
   // This works around a bug in "force update on reload".
   var refreshing;
-  navigator.serviceWorker.addEventListener('controllerchange', function() {
+  navigator.serviceWorker.addEventListener("controllerchange", function() {
     if (refreshing) return;
     window.location.reload();
     refreshing = true;
@@ -63,8 +71,8 @@ IndexController.prototype._registerServiceWorker = function() {
 
 IndexController.prototype._trackInstalling = function(worker) {
   var indexController = this;
-  worker.addEventListener('statechange', function() {
-    if (worker.state == 'installed') {
+  worker.addEventListener("statechange", function() {
+    if (worker.state == "installed") {
       indexController._updateReady(worker);
     }
   });
@@ -72,12 +80,12 @@ IndexController.prototype._trackInstalling = function(worker) {
 
 IndexController.prototype._updateReady = function(worker) {
   var toast = this._toastsView.show("New version available", {
-    buttons: ['refresh', 'dismiss']
+    buttons: ["refresh", "dismiss"]
   });
 
   toast.answer.then(function(answer) {
-    if (answer != 'refresh') return;
-    worker.postMessage({action: 'skipWaiting'});
+    if (answer != "refresh") return;
+    worker.postMessage({ action: "skipWaiting" });
   });
 };
 
@@ -87,36 +95,38 @@ IndexController.prototype._openSocket = function() {
   var latestPostDate = this._postsView.getLatestPostDate();
 
   // create a url pointing to /updates with the ws protocol
-  var socketUrl = new URL('/updates', window.location);
-  socketUrl.protocol = 'ws';
+  var socketUrl = new URL("/updates", window.location);
+  socketUrl.protocol = "ws";
 
   if (latestPostDate) {
-    socketUrl.search = 'since=' + latestPostDate.valueOf();
+    socketUrl.search = "since=" + latestPostDate.valueOf();
   }
 
   // this is a little hack for the settings page's tests,
   // it isn't needed for Wittr
-  socketUrl.search += '&' + location.search.slice(1);
+  socketUrl.search += "&" + location.search.slice(1);
 
   var ws = new WebSocket(socketUrl.href);
 
   // add listeners
-  ws.addEventListener('open', function() {
+  ws.addEventListener("open", function() {
     if (indexController._lostConnectionToast) {
       indexController._lostConnectionToast.hide();
     }
   });
 
-  ws.addEventListener('message', function(event) {
+  ws.addEventListener("message", function(event) {
     requestAnimationFrame(function() {
       indexController._onSocketMessage(event.data);
     });
   });
 
-  ws.addEventListener('close', function() {
+  ws.addEventListener("close", function() {
     // tell the user
     if (!indexController._lostConnectionToast) {
-      indexController._lostConnectionToast = indexController._toastsView.show("Unable to connect. Retrying…");
+      indexController._lostConnectionToast = indexController._toastsView.show(
+        "Unable to connect. Retrying…"
+      );
     }
 
     // try and reconnect in 5 seconds
@@ -129,12 +139,21 @@ IndexController.prototype._openSocket = function() {
 // called when the web socket sends message data
 IndexController.prototype._onSocketMessage = function(data) {
   var messages = JSON.parse(data);
+  console.log("this._dbPromise", this._dbPromise);
 
   this._dbPromise.then(function(db) {
     if (!db) return;
 
     // TODO: put each message into the 'wittrs'
     // object store.
+    let tx = db.transaction("wittrs", "readwrite");
+    let wittrStore = tx.objectStore("wittrs");
+    console.log("msg", messages);
+
+    messages.forEach(message => {
+      wittrStore.put(message);
+    });
+    // return tx.complete;
   });
 
   this._postsView.addPosts(messages);
